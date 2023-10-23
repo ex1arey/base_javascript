46fcfbcb28584cbebc91762f4f6332dd0ddb2ed2
Munris <munris.tech@gmail.com>
2023-10-23 20:54:12 +0300

    catch error

--- utils/getCurrentGas.ts
+++ utils/getCurrentGas.ts
@@ -15,12 +15,16 @@ export async function waitGas() {
     const logger = makeLogger("Gas checker")
     let isGoodGas = false
     while (!isGoodGas) {
-        const currentGas = await getCurrentGas()
-        if (currentGas > bridgeConfig.maxGas) {
-            logger.info(`Wait for gas ${bridgeConfig.maxGas}. Current gas: ${currentGas.toFixed(1)}`)
-            await sleep(10 * 1000)
-        } else {
-            return true
+        try {
+            const currentGas = await getCurrentGas()
+            if (currentGas > bridgeConfig.maxGas) {
+                logger.info(`Wait for gas ${bridgeConfig.maxGas}. Current gas: ${currentGas.toFixed(1)}`)
+                await sleep(10 * 1000)
+            } else {
+                return true
+            }
+        } catch (e) {
+            logger.error(`Error ${e.toString()}`)
         }
     }
 }
